a25de449e4131898a69c2b866422099ebc1c933a
Pedro Oliveira <pedro.henrike1215@gmail.com>
2025-06-25 13:42:36 -0300

    Alter baseApi

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: `${import.meta.env.VITE_API_URL}/`,
-  baseURL: "http://localhost:3000/",
+  baseURL: `${import.meta.env.VITE_API_URL}/`,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
